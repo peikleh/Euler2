0603213df13dd6b57936744421ad5c4ae2855d5c
Nevin <Nevin@Nevin_HP>
2014-05-13 20:04:54 -0600

    Program Works

---
+++ Euler2/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Nevin
+ *
+ * Created on May 13, 2014, 8:02 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
